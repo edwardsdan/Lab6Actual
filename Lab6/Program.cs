84d244bd752c8d4d1a915b9cf82392d1281f978b
DanE <edwardsdan001@gmail.com>
2018-01-27 21:13:11 -0500

    first commit of new, added new regex + methods

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -32,13 +32,29 @@ namespace Lab6
         public static void Print(string[] x)
         {
             char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
-            char[] punctuation = { '!', ',', '.', ';', ':', '?' };
 
             for (int i = 0; i < x.Length; i++)
             {
-                if (Regex.IsMatch(x[i], @"(\!|\,|\.|\;|\:|\?)$") == true)
+                if (Regex.IsMatch(x[i], "^[a-zA-Z\'\"\\!\\.\\?]+$"))
                 {
-                    x[i].LastIndexOfAny(punctuation); ////////// work in progress for removing and re-adding punctuation
+                    if (x[i].IndexOfAny(vowels) == 0)
+                    {
+                        Console.Write(ContainsPunctuation(StartsWithVowel(x[i])));
+                    }
+                    else if (string.IsNullOrWhiteSpace(x[i]) == true)
+                    {
+                        x[i] = x[i].Replace(" ", String.Empty);
+                    }
+                    else
+                    {
+                        Console.Write(ContainsPunctuation(StartsWithConsonant(x[i], vowels)));
+                    }
+
+                }
+                /*if (Regex.IsMatch(x[i], @"([^\s\w\'])") == true)
+                {
+                    int toRemove = x[i].LastIndexOfAny(punctuation); ////////// work in progress for removing and re-adding punctuation
+
                 }
                 if (Regex.IsMatch(x[i], @"^[a-zA-Z\']+$") == true)
                 {
@@ -49,22 +65,46 @@ namespace Lab6
                     }
                     else
                     {
-                        PrintConst(x, i, vowels);
+                        StartsWithConsonant(x, i, vowels);
                         continue;
                     }
                 }
                 else if (Regex.IsMatch(x[i], @"^[a-zA-Z\u0021-\u0040]+$") == true)
                 {
                     Console.Write(x[i] + " ");
-                }
+                }*/
             }
         }
-        
-        public static void PrintConst(string[] x, int i, char[] y)
+
+        public static string ContainsPunctuation(string x)
         {
-            int vowelIndex = x[i].IndexOfAny(y);
-            string toAppend = x[i].Substring(0, vowelIndex);
-            Console.Write(x[i].Remove(0, vowelIndex) + toAppend + "ay ");
+            char[] punct = { '!', '"', '.', '?', ':', ';' };
+            if (Regex.IsMatch(x, @"([\!\,\.\?\:\;])"))
+            {
+                int punctIndex = x.IndexOfAny(punct);
+                string addPunct = x.Substring(punctIndex, length: 1);
+                string final = $"{x.Remove(punctIndex, 1)}{addPunct}";
+                return final;
+            }
+            else
+            {
+                return x;
+            }
+        }
+
+        public static string StartsWithVowel(string x)
+        {
+            string toReturn = x + "way ";
+            return toReturn;
+        }
+        
+        public static string StartsWithConsonant(string x, char[] y)
+        {
+
+            int vowelIndex = x.IndexOfAny(y);
+            string toAppend = x.Substring(0, vowelIndex);
+            string toReturn = x.Remove(0, vowelIndex) + toAppend + "ay ";
+            return toReturn;
         }
 
         static void Main(string[] args)
