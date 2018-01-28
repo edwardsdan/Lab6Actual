96c8de2c9157a5a7535e09ac58f92ca7690ccc7c
Dan Edwards <35340235+edwardsdan@users.noreply.github.com>
2018-01-28 12:45:27 -0500

    Added classes, working on keeping case still

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@ using System.Threading.Tasks;
 
 namespace Lab6
 {
-    class Program
+    class Validation
     {
-        public static string ValidateInput ()
+        public static string ValidateInput()
         {
             bool loop = true;
             Console.WriteLine("Enter a word or phrase you want to translate!");
@@ -28,42 +28,19 @@ namespace Lab6
             }
             return x;
         }
+    }
 
-        public static string[] Print(string[] x)
-        {
-            char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
-
-            for (int i = 0; i < x.Length; i++)
-            {
-                if (Regex.IsMatch(x[i], "^[a-zA-Z\'\"\\!\\.\\?\\,]+$"))
-                {
-                    if (x[i].IndexOfAny(vowels) == 0)
-                    {
-                        x[i] = ContainsPunctuation(StartsWithVowel(x[i]));
-                    }
-                    else if (string.IsNullOrWhiteSpace(x[i]) == true)
-                    {
-                        x[i] = x[i].Replace(" ", String.Empty);
-                    }
-                    else
-                    {
-                        x[i] = ContainsPunctuation(StartsWithConsonant(x[i], vowels));
-                    }
-
-                }
-            }
-            return x;
-        }
-
+    class EditString
+    {
         public static string ContainsPunctuation(string x)
         {
             char[] punct = { '!', '"', '.', '?', ':', ';', ',' };
             if (Regex.IsMatch(x, @"([\!\,\.\?\:\;])"))
             {
-                int punctIndex = x.IndexOfAny(punct);
+                int punctIndex = x.IndexOfAny(punct); // similar to line 77
                 string addPunct = x.Substring(punctIndex, length: 1);
-                string final = $"{x.Remove(punctIndex, 1)}{addPunct}";
-                return final;
+                x = $"{x.Remove(punctIndex, 1)}{addPunct}";
+                return x;
             }
             else
             {
@@ -76,7 +53,7 @@ namespace Lab6
             string toReturn = x + "way";
             return toReturn;
         }
-        
+
         public static string StartsWithConsonant(string x, char[] y)
         {
 
@@ -85,13 +62,44 @@ namespace Lab6
             string toReturn = x.Remove(0, vowelIndex) + toAppend + "ay";
             return toReturn;
         }
+    }
+
+    class Program
+    {
+        public static string[] Print(string[] x)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' }; // see line 77
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (Regex.IsMatch(x[i], "^[a-zA-Z\'\"\\!\\.\\?\\,]+$"))
+                {
+                    if (x[i].IndexOfAny(vowels) == 0) // can probably use regex here, work in progress
+                    {
+                        x[i] = EditString.ContainsPunctuation(EditString.StartsWithVowel(x[i]));
+                    }
+                    else if (string.IsNullOrWhiteSpace(x[i]) == true)
+                    {
+                        x[i] = x[i].Replace(" ", String.Empty);
+                    }
+                    else
+                    {
+                        x[i] = EditString.ContainsPunctuation(EditString.StartsWithConsonant(x[i], vowels));
+                    }
+
+                }
+            }
+            return x;
+        }
+
+        
 
         static void Main(string[] args)
         {
             bool loop = true;
             while (loop == true)
             {
-                string[] Test = ValidateInput().Split(' ');
+                string[] Test = Validation.ValidateInput().Split(' ');
                 Console.WriteLine(string.Join(" ", Print(Test)));
 
                 Console.WriteLine();
